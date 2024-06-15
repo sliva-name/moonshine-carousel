76c3e92e0a23ee481af912bff030cbc7310d7a66
WebMatherFacker <antyuhov2@gmail.com>
2024-06-15 13:02:25 +0300

    add provider

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit7648b23f934ef7ced5c392459d54f233
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit7648b23f934ef7ced5c392459d54f233', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit7648b23f934ef7ced5c392459d54f233', 'loadClassLoader'));
@@ -30,6 +32,18 @@ class ComposerAutoloaderInit7648b23f934ef7ced5c392459d54f233
         call_user_func(\Composer\Autoload\ComposerStaticInit7648b23f934ef7ced5c392459d54f233::getInitializer($loader));
 
         $loader->register(true);
+
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit7648b23f934ef7ced5c392459d54f233::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
 
         return $loader;
     }
